18831a7972a0b77b93798b8e7bbf0cf416d18a15
enes turhan <enesturhan9@gmail.com>
2022-02-28 20:04:55 +0300

    Add project files.

---
+++ AgileManagement.Core/Authentication/IAuthenticationService.cs
@@ -0,0 +1,31 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+
+namespace AgileManagement.Core
+{
+
+    /// <summary>
+    /// Kullanıcın e-posta parola ve oturum açık kalsın seçeneğine göre sisteme giriş yapmasını oturum açmasını sağlayan servis. Bu servis üzerinden sisteme login olacağız. Task yazmamızın sebebi ise bir sistemde oturum açma istediği farklı servisler üzerinden olabilir Google,Facebook vs gibi bu durumda başka bir servise uygulamamız içerisinden bağlandığımız için bir müddet süre geçeceğinden asenkron bir iş yapılması daha doğtu olacaktır.
+    /// </summary>
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        //refresh token silme null icin set etmek icin kullanılabilir.
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        //5 taneye kadar app setting dosyasında tutulaiblir fazlası ise databasede tutmak mantıklıdır.
+        Response<ClientTokenDto> CreteTokenByClient(ClientLoginDto clientLoginDto);
+
+    }
+
+
+
+}
